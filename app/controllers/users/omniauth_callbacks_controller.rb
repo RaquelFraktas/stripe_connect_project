3b0adfda91d5f465a04800c0d8b39600632365bd
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-27 01:03:57 -0500

    add user bank to grabbed data

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
 
   # GET|POST /resource/auth/twitter
   def passthru
@@ -21,16 +15,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def stripe_connect
     auth_data = request.env["omniauth.auth"]
     @user = current_user
-    byebug
+
     if @user.persisted?
+      byebug
       @user.uid = auth_data.uid
       @user.provider = auth_data.provider
       @user.access_code = auth_data.credentials.token
       @user.publishable_key = auth_data.info.stripe_publishable_key
+      @user.bank= auth_data.extra.extra_info.external_accounts.data[0].bank_name
       @user.save
 
-      sign_in_and_redirect @user, event: authentication
-      flash[:notice] = "Stripe Account Connected" if is_navigationalç_format?
+      
+
+      sign_in_and_redirect @user, event: :authentication
+      flash[:notice] = "Stripe Account Connected" if is_navigational_format?
     else
       session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
       redirect_to root_path
