fef7f43a6af488d44a147b6eba821d194e13527c
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-26 20:28:30 -0500

    add stripe button and stripe url methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def stripe_url
+    # this might not work bc im using DOT ENV
+    "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write"
+  end
+
+  def stripe_connect_button # add this method
+    link_to stripe_url, class: "btn-stripe-connect" do
+      content_tag :span, "Connect with Stripe"
+  end
+
 end
