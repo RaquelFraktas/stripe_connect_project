568a8a67a1168e03261413a0a8285fed72cdfeda
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-26 23:31:56 -0500

    change quotes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def stripe_url
     # this might not work bc im using DOT ENV
-    `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{ENV["STRIPE_CONNECT_CLIENT_ID"]}&scope=read_write`
+    "https://connect.stripe.com/express/oauth/authorize?response_type=code&client_id=#{ENV["STRIPE_CONNECT_CLIENT_ID"]}&scope=read_write"
   end
 
   def stripe_connect_button
