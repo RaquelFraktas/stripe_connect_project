5b2c23f2b919e25545ca5d1e107023e9648ff9cb
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-26 20:28:13 -0500

    save stripe info once connected

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,9 +8,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def twitter
   # end
 
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
-
   # GET|POST /resource/auth/twitter
   def passthru
     super
@@ -22,9 +19,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def stripe_connect
-    # Delete the code inside of this method and write your own.
-    # The code below is to show you where to access the data.
-    raise request.env["omniauth.auth"].to_yaml
+    auth_data = request.env["omniauth.auth"]
+    @user = current_user
+    if @user.persisted?
+      @user.provider = auth_data.provider
+      @user.access_code = auth_data.credentials.token
+      @user.publishable_key = auth_data.info.stripe_publishable_key
+      @user.save
+
+      sign_in_and_redirect @user, event: authentication
+      flash [:notice] = "Stripe Account Connected" if is_navigational_format?
+    else
+      session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
+      redirect_to root_path
+    end
+  end
+
+  def failure
+    redirect_to root_path
   end
 end
 
