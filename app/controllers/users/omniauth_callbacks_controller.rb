d70c80b297f7d733734e47c73ef3e6e5d014ab84
Raquel Fraktas <72051783+RaquelFraktas@users.noreply.github.com>
2022-02-26 23:33:18 -0500

    add uid

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -21,14 +21,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def stripe_connect
     auth_data = request.env["omniauth.auth"]
     @user = current_user
+    byebug
     if @user.persisted?
+      @user.uid = auth_data.uid
       @user.provider = auth_data.provider
       @user.access_code = auth_data.credentials.token
       @user.publishable_key = auth_data.info.stripe_publishable_key
       @user.save
 
       sign_in_and_redirect @user, event: authentication
-      flash [:notice] = "Stripe Account Connected" if is_navigational_format?
+      flash[:notice] = "Stripe Account Connected" if is_navigationalç_format?
     else
       session["devise.stripe_connect_data"] = request.env["omniauth.auth"]
       redirect_to root_path
@@ -38,7 +40,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path
   end
-end
 
   # protected
 
